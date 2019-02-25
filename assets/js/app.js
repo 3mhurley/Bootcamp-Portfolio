2e2bc7b80e90808a8cd3eb6f1dce85cfb491b958
EH <33641969+3mhurley@users.noreply.github.com>
2019-02-24 21:04:49 -0700

    v1 final

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,59 +1,42 @@
+var slideIndex = 1;
+showSlides(slideIndex);
 
-var images = ['../images/hangman.png','../images/rock-paper-scissors.png'];
-var showImage;
-var count = 0;
-
-$('#start').click(function() {
-  startSlideshow();
-  // $('#image-holder').animate({
-  //   width: "70%",
-  //   opacity: 0.4,
-  //   marginLeft: "0.6in",
-  //   fontSize: "3em",
-  //   borderWidth: "10px"
-  // }, 1500 );
-});
-$('#stop').click(function() {
-  stopSlideshow();
-});
-
-
-function displayImage() {
-  $("#image-holder").html("<img src=" + images[count] + " width='400px'>").animate({
-    width: "100%",
-    opacity: 1,
-    marginRight: "0.6in",
-  }, 1500 );;
+// Next/previous controls
+function plusSlides(n) {
+    showSlides(slideIndex += n);
 }
 
-function nextImage() {
-
-  count++
-
-  $("#image-holder").html("<img src='images/loading.gif' width='400px'>").animate({
-    width: "70%",
-    opacity: 0.4,
-    marginLeft: "0.6in",
-    borderWidth: "10px"
-  }, 1500 );
-
-
-  setTimeout(displayImage, 1000);
-
-  if (count === images.length) {
-    count = 0;
-  }
-  
-}
-function startSlideshow() {
-
-  showImage = setInterval(nextImage, 1000);
-
-}
-function stopSlideshow() {
-
-  clearInterval(showImage);
-
+// Thumbnail image controls
+function currentSlide(n) {
+    showSlides(slideIndex = n);
 }
 
-displayImage();
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("demo");
+    var captionText = document.getElementById("caption");
+
+    if (n > slides.length) {
+      slideIndex = 1
+    }
+
+    if (n < 1) {
+      slideIndex = slides.length
+    }
+
+    for (i = 0; i < slides.length; i++) {
+       slides[i].style.display = "none";
+    }
+
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active";
+    captionText.innerHTML = dots[slideIndex-1].alt;
+
+    $(".prj-lnk").attr("href", dots[slideIndex-1].getAttribute('pl'));
+    $(".prj-src").attr("href", dots[slideIndex-1].getAttribute('ps'));
+}
